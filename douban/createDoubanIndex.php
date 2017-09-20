dfcef30daea447dac04c5742ce217a4ff55d98ae
万连波 <sting_bo@163.com>
2017-09-20 18:22:48 +0800

    创建douban index

---
+++ douban/createDoubanIndex.php
@@ -0,0 +1,59 @@
+<?php
+
+require __DIR__ . '/../start.php';
+
+use Elasticsearch\ClientBuilder;
+
+$client = ClientBuilder::create()->build();
+
+$params = [
+    'index' => 'douban',
+    'body' => [
+        'settings' => [
+            'number_of_shards' => 3,
+            'number_of_replicas' => 2
+        ],
+        'mappings' => [
+            'dd_book' => [
+                '_source' => [
+                    'enabled' => true
+                ],
+                'properties' => [
+                    'book_name' => [
+                        'type' => 'text',
+                        'analyzer' => 'ik_max_word',
+                        'search_analyzer' => 'ik_max_word',
+                    ],
+                    'book_author' => [
+                        'type' => 'text',
+                        'analyzer' => 'ik_max_word',
+                        'search_analyzer' => 'ik_max_word',
+                    ],
+                    'book_desc' => [
+                        'type' => 'text',
+                        'analyzer' => 'ik_max_word',
+                        'search_analyzer' => 'ik_max_word',
+                    ],
+                    'book_star' => [
+                        'type' => 'text',
+                    ],
+                    'book_pl' => [
+                        'type' => 'text',
+                    ],
+                    'book_publish' => [
+                        'type' => 'text',
+                    ],
+                    'book_date' => [
+                        'type' => 'text',
+                    ],
+                    'book_price' => [
+                        'type' => 'text',
+                    ],
+                ]
+            ]
+        ]
+    ]
+];
+
+$response = $client->indices()->create($params);
+print_r($response);
